8d563fbd5dc2d75a65160310eb54eebfd7e723a2
Maserall Marcelin <maserallm@knights.ucf.edu>
2019-12-14 19:42:09 -0500

    added choice functionality, semi user validation

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,8 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
+const chalk = require("chalk");
 
+let chosenItem = {};
 const connection = mysql.createConnection({
   host: "localhost",
 
@@ -26,6 +28,7 @@ function queryShop() {
         `${res[i].id} | ${res[i].product_name} | ${res[i].department_name} | ${res.price_consumer} | ${res[i].stock_quantity}`
       );
     }
+    runShop();
   });
 }
 
@@ -33,20 +36,57 @@ function runShop() {
   connection.query(`SELECT * FROM products`, (err, res) => {
     if (err) throw err;
     inquirer
-      .prompt({
-        type: "input",
-        message: "Enter the ID of the product you wish to purchase",
-        name: "id",
+      .prompt([
+        {
+          type: "input",
+          message: "Enter the ID of the product you wish to purchase",
+          name: "id"
+        },
+        {
+          type: "input",
+          message: "Enter the quantity you wish to purchase",
+          name: "quantity"
+        }
+      ])
+      .then(answer => {
+        let userItem = parseFloat(answer.id) - 1;
 
-        testIdEntry: function valid(value) {
-          if (value > 0 && value <= 10 && !isNaN(value)) {
-            return true;
+        if (userItem > res.length - 1 || userItem < 0) {
+          connection.end();
+          console.log(chalk.bold.red("Please Enter a Valid ID Number."));
+        } else {
+          if (
+            answer.quantity > res[userItem].stock_quantity ||
+            answer.quantity <= 0 ||
+            res[userItem].stock_quantity === 0
+          ) {
+            connection.end();
+            console.log(chalk.bold.red("Please Enter a Sufficient Quantity."));
           } else {
-            console.log("Please enter a value from 1 - 10. Thank You.");
-            return false;
+            chosenItem = {
+              id: res[userItem].id,
+              product_name: res[userItem].product_name,
+              department_name: res[userItem].department_name,
+              price_consumer: res[userItem].price_consumer,
+              stock_quantity: res[userItem].stock_quantity,
+              chosen_quantity: parseFloat(answer.quantity)
+            };
+            console.log(chosenItem);
+            updateStock();
           }
         }
-      })
-      .then(answer => {});
+      });
   });
+}
+
+function updateStock() {
+  let newStockQuantity = chosenItem.stock_quantity - chosenItem.chosen_quantity;
+  let id = chosenItem.id;
+  connection.query(
+    "UPDATE products SET ? WHERE ?",
+    [{ stock_quantity: newStockQuantity }, { id: id }],
+    function(err, res) {
+      if (err) throw err;
+    }
+  );
 }
